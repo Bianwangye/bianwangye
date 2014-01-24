aed5c29ffa4eb33dab7a81d0cdafd9a6f315ba66
chaixiangfei <chaixiangfei@hotmail.com>
2014-01-24 00:18:52 -0800

    first try
    test

---
+++ Auto List - millions of used cars and trucks for sale_files/geoip.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city()         { return 'Sunnyvale'; }
+function geoip_region()       { return 'CA'; }
+function geoip_region_name()  { return 'California'; }
+function geoip_latitude()     { return '37.3537'; }
+function geoip_longitude()    { return '-122.0307'; }
+function geoip_postal_code()  { return '94087'; }
+function geoip_area_code()    { return '408'; }
+function geoip_metro_code()   { return '807'; }
